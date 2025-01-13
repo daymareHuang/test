52902ea860acbe02967423e5ced3d1fe99a1f99f
daymareHuang <32323246+daymareHuang@users.noreply.github.com>
2025-01-13 15:11:36 +0800

    test mr 田

--- src/pages/closet/ClosetMatch.jsx
+++ src/pages/closet/ClosetMatch.jsx
@@ -43,7 +43,7 @@ function ClosetMatch() {
       <div className='row container'>
         {outfits.length > 0 ? (
           outfits.map((outfit) => (
-            <Link to={`http://localhost:5173/ClosetMatch/${outfit.OutfitID}`} key={outfit.OutfitID} className="col-6 pt-4 px-4 text-decoration-none text-dark">
+            <Link to={`/ClosetMatch/${outfit.OutfitID}`} key={outfit.OutfitID} className="col-6 pt-4 px-4 text-decoration-none text-dark">
               <div style={{filter: outfit.FilterStyle}} >
                 <p className="mb-1 ps-1 md-18">{outfit.Title}</p>
                 <img className="border rounded" src={outfit.EditedPhoto} width="160" height="220" alt="loading..." />
